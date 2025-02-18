914d9bb70c49c4914344a945e70b32b7495136a4
VillageR88 <villager88pl@gmail.com>
2025-02-18 13:44:53 +0100

    Initial test improvement
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,7 +1,8 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import fs from 'node:fs';
+import path from 'node:path';
 import Layout from '../app/layout';
-import Page from '../app/page';
 
 // Utility functions
 function checkAnchorElements(): void {
@@ -31,78 +32,96 @@ function checkImageElements(): void {
   }
 }
 
-describe('Page', () => {
-  // The Page component is tested to ensure that it renders the head, body, and main elements correctly.
-  // It also checks for the presence of images with alt attributes and anchor elements with valid href attributes and discernible text or aria-labels.
-  it('renders a head element', () => {
-    render(<Page />);
+// Dynamically import all page components
+const appDirectory = path.join(__dirname, '../app');
+const pageFiles = fs.readdirSync(appDirectory).filter((file) => file.match('page.tsx'));
 
-    const headElement = document.head;
+// Test each page component
+for (const file of pageFiles) {
+  describe(file, () => {
+    let PageComponent: () => JSX.Element;
 
-    expect(headElement).toBeInTheDocument();
+    beforeAll(async () => {
+      const importedModule = (await import(path.join(appDirectory, file))) as { default: () => JSX.Element };
+      PageComponent = importedModule.default;
+    });
+    it('renders a head element', () => {
+      render(
+        <Layout>
+          <PageComponent />
+        </Layout>,
+      );
+
+      const headElement = document.head;
+
+      expect(headElement).toBeInTheDocument();
+    });
+
+    it('renders a body element', () => {
+      render(
+        <Layout>
+          <PageComponent />
+        </Layout>,
+      );
+
+      const bodyElement = document.body;
+
+      expect(bodyElement).toBeInTheDocument();
+    });
+
+    it('renders a main element', () => {
+      render(
+        <Layout>
+          <PageComponent />
+        </Layout>,
+      );
+
+      const mainElement = screen.getByRole('main');
+
+      expect(mainElement).toBeInTheDocument();
+    });
+
+    it('renders image elements with alt attributes if any', () => {
+      render(
+        <Layout>
+          <PageComponent />
+        </Layout>,
+      );
+      checkImageElements();
+    });
+
+    it('renders anchor elements with non-empty href attributes and discernible text or aria-label if any', () => {
+      render(
+        <Layout>
+          <PageComponent />
+        </Layout>,
+      );
+      checkAnchorElements();
+    });
+
+    it('renders the navigation and footer elements', () => {
+      render(
+        <Layout>
+          <PageComponent />
+        </Layout>,
+      );
+
+      const navElement = screen.getByRole('navigation');
+      const footerElement = screen.getByRole('contentinfo');
+
+      expect(navElement).toBeInTheDocument();
+      expect(footerElement).toBeInTheDocument();
+    });
+
+    it('renders the logo in the header and footer', () => {
+      render(
+        <Layout>
+          <PageComponent />
+        </Layout>,
+      );
+
+      const logos = screen.getAllByAltText('logo');
+      expect(logos.length).toBe(2);
+    });
   });
-
-  it('renders a body element', () => {
-    render(<Page />);
-
-    const bodyElement = document.body;
-
-    expect(bodyElement).toBeInTheDocument();
-  });
-
-  it('renders a main element', () => {
-    render(<Page />);
-
-    const mainElement = screen.getByRole('main');
-
-    expect(mainElement).toBeInTheDocument();
-  });
-
-  it('renders image elements with alt attributes if any', () => {
-    render(<Page />);
-    checkImageElements();
-  });
-
-  it('renders anchor elements with non-empty href attributes and discernible text or aria-label if any', () => {
-    render(<Page />);
-    checkAnchorElements();
-  });
-});
-
-describe('Layout', () => {
-  // The Layout component is tested to ensure that it renders the navigation and footer elements correctly.
-  // It also checks for the presence of the logo and navigation links in both the header and footer.
-  it('renders the navigation and footer elements', () => {
-    render(
-      <Layout>
-        <div />
-      </Layout>,
-    );
-
-    const navElement = screen.getByRole('navigation');
-    const footerElement = screen.getByRole('contentinfo');
-
-    expect(navElement).toBeInTheDocument();
-    expect(footerElement).toBeInTheDocument();
-  });
-
-  it('renders the logo in the header and footer', () => {
-    render(
-      <Layout>
-        <div />
-      </Layout>,
-    );
-
-    const logos = screen.getAllByAltText('logo');
-    expect(logos.length).toBe(2);
-  });
-
-  it('renders anchor elements with non-empty href attributes and discernible text or aria-label if any', () => {
-    render(
-      <Layout>
-        <div />
-      </Layout>,
-    );
-    checkAnchorElements();
-  });
-});
+}
