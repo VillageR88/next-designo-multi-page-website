a14f5def1b48065bb3674178a556713acf013f33
VillageR88 <villager88pl@gmail.com>
2025-02-21 23:09:21 +0100

    Initial design route responsive design
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- components/DesignRoute.tsx
+++ components/DesignRoute.tsx
@@ -5,14 +5,14 @@ import Image from 'next/image';
 import SectionDesign from '@/components/SectionDesign';
 
 const DesignRoute = ({ pageFiles, relativeDirname }: { pageFiles: string[]; relativeDirname: string }): JSX.Element => (
-  <main className="mb-[160px] mt-[64px] flex flex-col gap-[160px] px-[39px] [background:no-repeat_calc(50%-12.5em)_126px_url(../public/assets/shared/desktop/bg-pattern-leaf.svg)]">
-    <div className="mx-auto flex min-h-[252px] w-full max-w-[69.375em] items-center justify-center rounded-[15px] [background:no-repeat_100%_50%_url(../public/assets/web-design/desktop/bg-pattern-intro-web.svg)_#E7816B] [padding:64px_191px]">
+  <main className="mb-[160px] mt-[64px] flex flex-col gap-[160px] px-[39px] screen1024:[background:no-repeat_calc(50%-12.5em)_126px_url(../public/assets/shared/desktop/bg-pattern-leaf.svg)]">
+    <div className="mx-auto flex min-h-[320px] w-full max-w-[69.375em] items-center justify-center overflow-x-clip rounded-[15px] [background:no-repeat_100%_50%_url(../public/assets/web-design/desktop/bg-pattern-intro-web.svg)_#E7816B] [padding:64px_191px] md:min-h-[252px]">
       <section className="flex max-w-[400px] flex-col items-center justify-center gap-[24px] text-center">
         <h1 className="text-[48px] font-medium leading-[48px] text-white">{designRouteItems[relativeDirname].title}</h1>
         <p className="leading-[26px] text-white">{designRouteItems[relativeDirname].description}</p>
       </section>
     </div>
-    <ul className="mx-auto grid min-h-[478px] w-full max-w-[69.375em] grid-cols-3 gap-[32px_30px]">
+    <ul className="mx-auto flex min-h-[478px] w-full max-w-[69.375em] grid-cols-3 flex-col gap-[32px_30px] screen1024:grid">
       {pageFiles.map((item) => {
         const imagePath = path.join('/assets', relativeDirname, 'desktop', item);
         const key = item.match(/(?<=image-).+(?=.jpg)/)?.[0];
@@ -21,11 +21,17 @@ const DesignRoute = ({ pageFiles, relativeDirname }: { pageFiles: string[]; rela
         return (
           <li key={key} className="flex flex-col rounded-[15px]">
             <Link
-              className="[transition:transform_600ms] hover:scale-110 [&:hover>div]:bg-[#E7816B] [&:hover>div_h2]:text-[white] [&:hover>div_p]:text-[white] [&>div]:bg-[#FDF3F0] [&>div_h2]:text-[#E7816B] [&>div_p]:text-[#333136]"
+              className="flex flex-col [transition:transform_600ms] hover:scale-105 md:flex-row screen1024:block hover:screen1024:scale-110 [&:hover>div]:bg-[#E7816B] [&:hover>div_h2]:text-[white] [&:hover>div_p]:text-[white] [&>div]:bg-[#FDF3F0] [&>div_h2]:text-[#E7816B] [&>div_p]:text-[#333136]"
               href="#"
             >
-              <Image className="rounded-t-[15px]" alt={item} width={350} height={320} src={imagePath} />
-              <div className="flex min-h-[158px] items-center justify-center rounded-b-[15px] p-[32px] ease-in-out [transition:background-color_600ms]">
+              <Image
+                className="w-full rounded-[15px_15px_0_0] md:max-w-[339px] md:rounded-[15px_0_0_15px] screen1024:max-w-full screen1024:rounded-[15px_15px_0_0]"
+                alt={item}
+                width={350}
+                height={320}
+                src={imagePath}
+              />
+              <div className="flex min-h-[158px] w-full items-center justify-center rounded-[0_0_15px_15px] p-[32px] ease-in-out [transition:background-color_600ms] md:rounded-[0_15px_15px_0] screen1024:rounded-[0_0_15px_15px] screen1024:p-[32px_14px] xl:p-[32px]">
                 <section className="flex flex-col items-center justify-center gap-[16px] text-center">
                   <h2 className="text-[20px] font-medium leading-[26px] tracking-[5px] [transition:color_300ms]">
                     {key.toUpperCase()}
